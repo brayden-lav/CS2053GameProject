ffb1535d51c5e9f43dbad6eedfe59e09d4cc1552
Brayden Lavigne <brayden_lavigne@hotmail.com>
2026-04-04 21:22:00 -0300

    adding ruby's script

---
+++ GemWarsFile/ruby.gd
@@ -0,0 +1,61 @@
+extends CharacterBody2D
+ 
+@export var speed := 30
+@export var next_scene: String = "res://fightning_ruby_1.tscn"
+ 
+var direction := -1
+var target: CharacterBody2D = null
+var state := "patrol"
+ 
+@onready var anim: AnimatedSprite2D = $frames
+@onready var idle_timer: Timer = $IdleTimer
+ 
+func _ready():
+	idle_timer.wait_time = 5.0
+	idle_timer.one_shot = true
+	idle_timer.start()
+ 
+func _physics_process(delta):
+	match state:
+		"patrol":
+			patrol()
+		"idle":
+			idle()
+ 
+ 
+func patrol():
+	velocity.x = direction * speed
+	move_and_slide()
+ 
+	if is_on_wall():
+		direction *= -1
+ 
+		update_animation()
+ 
+func idle():
+	velocity = Vector2.ZERO
+	move_and_slide()
+	anim.play("Idle")
+ 
+ 
+func update_animation():
+	if velocity.x != 0:
+		anim.flip_h = velocity.x > 0
+		anim.play("LeftWalk")
+	else:
+		anim.play("Idle")
+ 
+func _on_idle_timer_timeout() -> void:
+	state = "idle"
+	anim.play("Idle")
+ 
+# Wait 2 seconds
+	await get_tree().create_timer(2.0).timeout
+ 
+	state = "patrol"
+	idle_timer.start()
+ 
+ 
+func _on_transfer_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		get_tree().change_scene_to_file(next_scene)
