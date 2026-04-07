639285ec2baffdd28e19d71376aa70e0224ccbed
Justin <justineziefule.49@unb.ca>
2026-04-07 19:37:15 -0300

    Playthrough finally done i'm tired

--- GemWarsFile/Scripts/main.gd
+++ GemWarsFile/Scripts/main.gd
@@ -4,8 +4,9 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	GameManager.play_music("res://Music/Tension in the House.mp3")
-	var dialogue = preload("res://Scene/dialog_box.tscn").instantiate()
-	add_child(dialogue)
+	if !GameManager.level_1_completed:
+		var dialogue = preload("res://Scene/dialog_box.tscn").instantiate()
+		add_child(dialogue)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
